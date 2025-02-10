27e30f0bc750fb8e9815bb7992b76a49d63394e1
mohamed hany <me5127538@gmail.com>
2025-02-10 03:46:09 +0200

    Initial commit

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,31 @@
+
+#pragma warning(disable: 4996)
+
+#include <iostream>
+#include <stack> // in STL (Standard Template Library)
+#include <vector> // in STL (Standard Template Library)
+#include <queue> // in STL (Standard Template Library)
+#include <map> // in STL (Standard Template Library)
+#include <string>
+#include "clsQueueLine.h"
+
+
+
+using namespace std;
+// shortcuts
+// remove line (ctrl + l)
+// solution explorer (ctrl + alt + l)
+// add new header file (local library) (ctrl + shift + a)
+
+
+int main() {
+
+
+
+
+
+
+
+
+	return 0;
+}
